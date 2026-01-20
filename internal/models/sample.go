d42094651f2da6a8fed179c39798758145809f21
Ngakan Gandhi <ngandhi@pmintl.net>
2026-01-20 11:17:12 +0700

    feat: add Topic and TopicListResponse struct

--- internal/models/sample.go
+++ internal/models/sample.go
@@ -18,3 +18,19 @@ type Sample struct {
 	Payload     []byte      `json:"payload"`
 	Timestamp   time.Time   `json:"timestamp"`
 }
+
+// Topic is the database model
+type Topic struct {
+	ID            int64       `json:"id"             db:"id"`
+	BrokerID      string      `json:"broker_id"      db:"broker_id"`
+	Topic         string      `json:"topic"          db:"topic"`
+	PayloadType   PayloadType `json:"payload_type"   db:"payload_type"`
+	SamplePayload []byte      `json:"sample_payload" db:"sample_payload"`
+	LastSeen      time.Time   `json:"last_seen"      db:"last_seen"`
+	CreatedAt     time.Time   `json:"created_at"     db:"created_at"`
+}
+
+type TopicListResponse struct {
+	Topics []Topic `json:"topics"`
+	Total  int     `json:"total"`
+}
